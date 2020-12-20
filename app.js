4b39db4889e0ddf345c22da109123d1ea754f0d9
Marius Thøgersen <marius91@live.dk>
2020-12-20 21:12:00 +0100

    pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Again World!'));
+app.get('/', (req, res) => res.send('Deployment pipeline created and working!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
